7ad8b35a31659f8242c6ee91a7ad1747c02f0089
hashms0a <47391615+hashms0a@users.noreply.github.com>
2025-03-09 00:12:04 +0300

    Update block_dataset_gallery.py
    Trigger loading images when needed, e.g., on an event or button click.

--- scripts/ui/block_dataset_gallery.py
+++ scripts/ui/block_dataset_gallery.py
@@ -1,40 +1,46 @@
-from __future__ import annotations
 from typing import TYPE_CHECKING, Callable
-
 import gradio as gr
-
+from PIL import Image
 from .ui_common import *
 from .uibase import UIBase
+import os
 
 if TYPE_CHECKING:
     from .ui_classes import *
-Filter = dte_module.filters.Filter
 
+Filter = dte_module.filters.Filter
 
 class DatasetGalleryUI(UIBase):
     def __init__(self):
         self.selected_path = ""
         self.selected_index = -1
 
-    def create_ui(self, image_columns, get_filters: Callable[[], list[Filter]]):
+    def create_ui(self, image_columns: int, get_filters: Callable[[], list[Filter]]):
         self.gl_dataset_images = gr.Gallery(
-            label="Dataset Images", elem_id="dataset_gallery", columns=image_columns)
+            label="Dataset Images", elem_id="dataset_gallery", columns=image_columns, preview=True)
         self.get_filters = get_filters
 
-    def set_callbacks(
-        self,
-        gallery_state: GalleryStateUI
-    ):
+    def set_callbacks(self, gallery_state: 'GalleryStateUI'):
+        
         def gl_dataset_images_on_change(select_data: gr.SelectData):
-            if select_data.selected:
-                imgs = dte_instance.get_filtered_imgpaths(self.get_filters())
+            imgs = dte_instance.get_filtered_imgpaths(self.get_filters())
+            valid_imgs = [img for img in imgs if os.path.isfile(img)]
+            if select_data.selected and 0 <= select_data.index < len(valid_imgs):
                 self.selected_index = select_data.index
-                self.selected_path = imgs[self.selected_index]
+                self.selected_path = valid_imgs[self.selected_index]
             else:
                 self.selected_index = -1
                 self.selected_path = ""
             gallery_state.register_value("Selected Image", self.selected_path)
-        
-        self.gl_dataset_images.select(gl_dataset_images_on_change)
 
-    
+        def load_images_to_gallery():
+            imgs = dte_instance.get_filtered_imgpaths(self.get_filters())
+            valid_imgs = [img for img in imgs if os.path.isfile(img)]
+            return [[Image.open(img), os.path.basename(img)] for img in valid_imgs]
+
+        # Assigning callbacks
+        self.gl_dataset_images.select(gl_dataset_images_on_change)
+        
+        # Trigger loading images when needed, e.g., on an event or button click.
+        load_button = gr.Button("Load Images")
+        load_button.click(fn=load_images_to_gallery, inputs=[], outputs=self.gl_dataset_images)
